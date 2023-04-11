6880ebff649508e8343c456faee225f94d033e32
Robert Bragg <robert@sixbynine.org>
2023-04-11 12:50:52 +0100

    Re-work event loop run() API so it can return a Result
    This re-works the portable `run()` API that consumes the `EventLoop` and
    runs the loop on the calling thread until the app exits.
    
    This can be supported across _all_ platforms and compared to the
    previous `run() -> !` API is now able to return a `Result` status on all
    platforms except iOS and Web. Fixes: #2709
    
    By moving away from `run() -> !` we stop calling `std::process::exit()`
    internally as a means to kill the process without returning which means
    it's possible to return an exit status and applications can return from
    their `main()` function normally.
    
    This also fixes Android support where an Activity runs in a thread but
    we can't assume to have full ownership of the process (other services
    could be running in separate threads).
    
    Additionally all examples have generally been updated so that `main()`
    returns a `Result` from `run()`
    
    Fixes: #2709

--- examples/resizable.rs
+++ examples/resizable.rs
@@ -12,7 +12,7 @@ use winit::{
 #[path = "util/fill.rs"]
 mod fill;
 
-fn main() {
+fn main() -> std::result::Result<(), impl std::error::Error> {
     SimpleLogger::new().init().unwrap();
     let event_loop = EventLoop::new();
 
@@ -53,5 +53,5 @@ fn main() {
             }
             _ => (),
         };
-    });
+    })
 }
--- examples/window_debug.rs
+++ examples/window_debug.rs
@@ -14,7 +14,7 @@ use winit::{
 #[path = "util/fill.rs"]
 mod fill;
 
-fn main() {
+fn main() -> std::result::Result<(), impl std::error::Error> {
     SimpleLogger::new().init().unwrap();
     let event_loop = EventLoop::new();
 
@@ -139,5 +139,5 @@ fn main() {
             }
             _ => (),
         }
-    });
+    })
 }
